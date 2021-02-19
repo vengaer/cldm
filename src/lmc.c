16f1d15ee48efdaad3a067fde118cba8bdac6038
Vilhelm <vilhelm.engstrom@tuta.io>
2021-02-19 17:16:04 +0100

    Add libmockc

---
+++ src/lmc.c
@@ -0,0 +1,32 @@
+#include "lmc.h"
+
+static void *dlhandle;
+struct lmc_mock_info *mockinfo;
+
+int lmc_init1(char const *binary_path) {
+    dlhandle = dlopen(binary_path, RTLD_LAZY);
+    if(!dlhandle) {
+        char const *err = dlerror();
+        lmc_assert(!err, err);
+    }
+    return 0;
+}
+
+int lmc_init2(int argc, char const **argv) {
+    (void)argc;
+    return lmc_init1(argv[1]);
+}
+
+void lmc_close(void) {
+    if(dlhandle) {
+        dlclose(dlhandle);
+    }
+}
+
+_Bool lmc_is_open(void) {
+    return dlhandle;
+}
+
+void *lmc_symbol(char const *symname) {
+    return dlsym(dlhandle, symname);
+}
