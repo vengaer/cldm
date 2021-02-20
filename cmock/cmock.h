36b1c7350ae09a2dc225ad103de9e2e10800fefa
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-02-20 22:53:21 +0100

    Fix issue where only 126 parameters were supported

--- cmock/cmock.h
+++ cmock/cmock.h
@@ -8,22 +8,23 @@
 
 #include <dlfcn.h>
 
-#define cmock_count_sequence 127, 126, 125, 124, 123, 122, 121, 120,    \
-                             119, 118, 117, 116, 115, 114, 113, 112,    \
-                             111, 110, 109, 108, 107, 106, 105, 104,    \
-                             103, 102, 101, 100, 99,  98,  97,  96,     \
-                             95,  94,  93,  92,  91,  90,  89,  88,     \
-                             87,  86,  85,  84,  83,  82,  81,  80,     \
-                             79,  78,  77,  76,  75,  74,  73,  72,     \
-                             71,  70,  69,  68,  67,  66,  65,  64,     \
-                             63,  62,  61,  60,  59,  58,  57,  56,     \
-                             55,  54,  53,  52,  51,  50,  49,  48,     \
-                             47,  46,  45,  44,  43,  42,  41,  40,     \
-                             39,  38,  37,  36,  35,  34,  33,  32,     \
-                             31,  30,  29,  28,  27,  26,  25,  24,     \
-                             23,  22,  21,  20,  19,  18,  17,  16,     \
-                             15,  14,  13,  12,  11,  10,  9,   8,      \
-                             7,   6,   5,   4,   3,   2,   1,   0
+#define cmock_count_sequence 128, 127, 126, 125, 124, 123, 122, 121,    \
+                             120, 119, 118, 117, 116, 115, 114, 113,    \
+                             112, 111, 110, 109, 108, 107, 106, 105,    \
+                             104, 103, 102, 101, 100, 99,  98,  97,     \
+                             96,  95,  94,  93,  92,  91,  90,  89,     \
+                             88,  87,  86,  85,  84,  83,  82,  81,     \
+                             80,  79,  78,  77,  76,  75,  74,  73,     \
+                             72,  71,  70,  69,  68,  67,  66,  65,     \
+                             64,  63,  62,  61,  60,  59,  58,  57,     \
+                             56,  55,  54,  53,  52,  51,  50,  49,     \
+                             48,  47,  46,  45,  44,  43,  42,  41,     \
+                             40,  39,  38,  37,  36,  35,  34,  33,     \
+                             32,  31,  30,  29,  28,  27,  26,  25,     \
+                             24,  23,  22,  21,  20,  19,  18,  17,     \
+                             16,  15,  14,  13,  12,  11,  10,  9,      \
+                             8,   7,   6,   5,   4,   3,   2,   1,      \
+                             0
 
 #define cmock_count_pick(a0,   a1,   a2,   a3,   a4,   a5,   a6,   a7,      \
                          a8,   a9,   a10,  a11,  a12,  a13,  a14,  a15,     \
@@ -41,7 +42,7 @@
                          a104, a105, a106, a107, a108, a109, a110, a111,    \
                          a112, a113, a114, a115, a116, a117, a118, a119,    \
                          a120, a121, a122, a123, a124, a125, a126, a127,    \
-                         ...) a127
+                         a128, ...) a128
 
 #define cmock_count_expand(...) cmock_count_pick(__VA_ARGS__)
 #define cmock_count(...) cmock_count_expand(__VA_ARGS__, cmock_count_sequence)
@@ -438,6 +439,7 @@
 #define cmock_mock_function125(...) cmock_mock_function(__VA_ARGS__)
 #define cmock_mock_function126(...) cmock_mock_function(__VA_ARGS__)
 #define cmock_mock_function127(...) cmock_mock_function(__VA_ARGS__)
+#define cmock_mock_function128(...) cmock_mock_function(__VA_ARGS__)
 
 #define cmock_assert1(...)   cmock_assert_no_msg(__VA_ARGS__)
 #define cmock_assert2(...)   cmock_assert_msg(__VA_ARGS__)
@@ -764,9 +766,9 @@ enum cmock_opmode {
 
 #define CMOCK_MOCK_FUNCTION(rettype, ...)                                                                           \
     cmock_cat_expand(cmock_mock_function, cmock_count(__VA_ARGS__))(rettype cmock_cat_expand(cmock_rv_, __LINE__),  \
-                                                              cmock_cat_expand(cmock_rv_, __LINE__) =,              \
-                                                              return cmock_cat_expand(cmock_rv_, __LINE__),         \
-                                                              rettype, rettype, __VA_ARGS__)
+                                                                    cmock_cat_expand(cmock_rv_, __LINE__) =,        \
+                                                                    return cmock_cat_expand(cmock_rv_, __LINE__),   \
+                                                                    rettype, rettype, __VA_ARGS__)
 
 #define CMOCK_MOCK_FUNCTION0(rettype, ...) \
     cmock_cat_expand(cmock_mock_function0_, cmock_count(__VA_ARGS__))(rettype, __VA_ARGS__)
