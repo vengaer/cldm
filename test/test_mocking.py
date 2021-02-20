284521772524f3ba808a59e6ad164545a5b6af22
Vilhelm <vilhelm.engstrom@tuta.io>
2021-02-20 10:04:41 +0100

    Add test for gmock-compatible syntax

--- test/test_mocking.py
+++ test/test_mocking.py
@@ -36,3 +36,34 @@ def test_preload():
 
     assert os.system('make -C {}'.format(working_dir)) == 0
     assert os.system('LD_PRELOAD={} LD_LIBRARY_PATH={} {}'.format(solib, working_dir, working_dir / target))
+
+def test_gmock_compat():
+    target = 'makegen_test'
+    symfile = 'syms.c'
+
+    db = read_db()
+    cgen = CGen('main.c')
+    cgen.append_line('#define LMC_GMOCK_COMPAT')
+    cgen.append_include('lmc.h')
+    cgen.append_include('syms.h', system_header=False)
+    with cgen.with_open_function('int', 'main'):
+        cgen.append_line('EXPECT_CALL(bar).WillRepeatedly(Return(28));')
+        cgen.append_return(CGen.default_call('bar', db['symbols']['bar']['params']))
+    cgen.write()
+
+    cgen = CGen(symfile)
+    cgen.generate_matching_symbols()
+    cgen.write()
+
+    mgen = Makegen(target, src='main.c')
+    mgen.adjust('CFLAGS', '-fPIC', Mod.REMOVE)
+    mgen.adjust('LDFLAGS', '-shared', Mod.REMOVE)
+    mgen.adjust('LDFLAGS', '-rdynamic -L. -L{} -lmockc'.format(project_root), Mod.APPEND)
+    mgen.adjust('LDLIBS', '-ltest', Mod.APPEND)
+    mgen.add_rule('libtest.so', '$(builddir)/syms.o', '$(QUIET)$(CC) -o $@ $^ -shared $(LDFLAGS) $(LDLIBS)', '[LD] $@')
+    mgen.add_rule('$(builddir)/syms.o', str(working_dir / symfile), '$(QUIET)$(CC) -o $@ $^ $(CFLAGS) $(CPPFLAGS) -fPIC', '[CC] $@')
+    mgen.add_prereq(target, 'libtest.so')
+    mgen.generate()
+
+    assert os.system('make -C {}'.format(working_dir)) == 0
+    assert os.system('LD_PRELOAD={} LD_LIBRARY_PATH={} {}'.format(solib, working_dir, working_dir / target))
