1b60b4c39d4f500a15d5f0a91693778211b5c595
Vilhelm <vilhelm.engstrom@tuta.io>
2021-02-20 18:26:47 +0100

    Add unit test for third readme example

--- test/test_readme.py
+++ test/test_readme.py
@@ -68,3 +68,37 @@ def test_readme_ex1():
     assert exec_bash('gcc -shared -fPIC -o {d}/libresource.so {d}/resource.c'.format(d=working_dir))[0] == 0
     assert exec_bash('gcc -o {d}/a.out {d}/main.c -L{d} -L{root} -lresource -lmockc -I{root}/libmockc'.format(d=working_dir, root=project_root))[0] == 0
     assert exec_bash('LD_PRELOAD={root}/libmockc.so LD_LIBRARY_PATH={wd} {wd}/a.out'.format(root=project_root, wd=working_dir))[0] == 0
+
+def test_readme_ex2():
+    target = 'readme_test'
+
+    cgen = CGen(symfile)
+    cgen.append_include('lmc.h', system_header=False)                           \
+        .append_line('MOCK_FUNCTION(int *, get_resource, int);')
+    cgen.write()
+
+    cgen = CGen('resource.c')
+    cgen.append_line('static int resource_array[32] = { 1, 2, 3 };')
+    with cgen.with_open_function('int *', 'get_resource', ['int']):
+        cgen.append_return('&resource_array[a0]')
+    cgen.write()
+
+    cgen = CGen('resource.h')
+    cgen.append_line('int *get_resource(int a0);')                              \
+        .write()
+
+    cgen = CGen('main.c')
+    cgen.append_include('lmc.h', system_header=False)                           \
+        .append_include('resource.h', system_header=False)                      \
+        .append_include('assert.h')
+    with cgen.with_open_function('int', 'main'):
+        cgen.append_line('int i = 12;')                                         \
+            .append_line('EXPECT_CALL(get_resource).WILL_ONCE(RETURN(&i));')    \
+            .append_line('int *res = get_resource(0);')                         \
+            .append_line('assert(res == &i);')                                  \
+            .append_return(0)
+    cgen.write()
+
+    assert exec_bash('make -B -C {}'.format(project_root))[0] == 0
+    assert exec_bash('gcc -o {d}/a.out {d}/main.c {d}/resource.c -L{d} -L{root} -lmockc -I{root}/libmockc'.format(d=working_dir, root=project_root))[0] == 0
+    assert exec_bash('LD_PRELOAD={root}/libmockc.so LD_LIBRARY_PATH={wd} {wd}/a.out'.format(root=project_root, wd=working_dir))[0] != 0
