62ab5b389392f9245f0bef3ad6b6bae770e5d3b3
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-03-05 20:34:22 +0100

    Move general macros to cldm_macro.h

--- cldm/cldm_mem.h
+++ cldm/cldm_mem.h
@@ -5,9 +5,6 @@
 
 #include <stddef.h>
 
-#define cldm_alignof(type)  \
-    (size_t)&(((struct cldm_cat_expand(cldm_align,__LINE__) { char b; type t; } *)0)->t)
-
 void *cldm_mmove(void *dst, void const* restrict src, size_t size);
 void *cldm_mcpy(void *restrict dst, void const* restrict src, size_t size);
 
