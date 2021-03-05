43d74aa51de4090988166c1d340c28d45227fc0c
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-03-05 20:00:59 +0100

    Add mmove and mcpy implementations

---
+++ cldm/cldm_mem.c
@@ -0,0 +1,63 @@
+#include "cldm_mem.h"
+
+union dmemacc {
+    unsigned char *byte;
+    unsigned *dword;
+};
+
+union smemacc {
+    unsigned char const* byte;
+    unsigned const *dword;
+};
+
+void *cldm_mmove(void *dst, void const* src, size_t size) {
+    union dmemacc d = {
+        .byte = dst
+    };
+    union smemacc s = {
+        .byte = src
+    };
+
+    size_t align = cldm_alignof(unsigned);
+    unsigned i = 0;
+
+    for(; i < size && ((((size_t)d.byte) % align) || (((size_t)s.byte) % align)); i++) {
+        *d.byte++ = *s.byte++;
+    }
+
+    for(; i + sizeof(*s.dword) <= size; i += sizeof(*s.dword)) {
+        *d.dword++ = *s.dword++;
+    }
+
+    for(; i < size; i++) {
+        *d.byte++ = *s.byte++;
+    }
+
+    return dst;
+}
+
+void *cldm_mcpy(void *restrict dst, void const *restrict src, size_t size) {
+    union dmemacc d = {
+        .byte = dst
+    };
+    union smemacc s = {
+        .byte = src
+    };
+
+    size_t align = cldm_alignof(unsigned);
+    unsigned i = 0;
+
+    for(; i < size && ((((size_t)d.byte) % align) || (((size_t)s.byte) % align)); i++) {
+        *d.byte++ = *s.byte++;
+    }
+
+    for(; i + sizeof(*s.dword) <= size; i += sizeof(*s.dword)) {
+        *d.dword++ = *s.dword++;
+    }
+
+    for(; i < size; i++) {
+        *d.byte++ = *s.byte++;
+    }
+
+    return dst;
+}
--- cldm/cldm_mem.h
+++ cldm/cldm_mem.h
@@ -8,6 +8,8 @@
 #define cldm_alignof(type)  \
     (size_t)&(((struct cldm_cat_expand(cldm_align,__LINE__) { char b; type t; } *)0)->t)
 
+void *cldm_mmove(void *dst, void const* restrict src, size_t size);
+void *cldm_mcpy(void *restrict dst, void const* restrict src, size_t size);
 
 
 #endif /* CLDM_MEM_H */
