0a8d3574a7705ee92245c4d078035f48a6a20eb0
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-03-05 20:32:05 +0100

    Reduce code duplication in mmove/mcpy

--- cldm/cldm_mem.c
+++ cldm/cldm_mem.c
@@ -1,5 +1,28 @@
 #include "cldm_mem.h"
 
+#define cldm_copy_bytes(dst, src, size)                                                     \
+    union dmemacc d = {                                                                     \
+        .byte = dst                                                                         \
+    };                                                                                      \
+    union smemacc s = {                                                                     \
+        .byte = src                                                                         \
+    };                                                                                      \
+                                                                                            \
+    size_t align = cldm_alignof(unsigned);                                                  \
+    unsigned i = 0;                                                                         \
+                                                                                            \
+    for(; i < size && ((((size_t)d.byte) % align) || (((size_t)s.byte) % align)); i++) {    \
+        *d.byte++ = *s.byte++;                                                              \
+    }                                                                                       \
+    for(; i + sizeof(*s.dword) <= size; i += sizeof(*s.dword)) {                            \
+        *d.dword++ = *s.dword++;                                                            \
+    }                                                                                       \
+    for(; i < size; i++) {                                                                  \
+        *d.byte++ = *s.byte++;                                                              \
+    }                                                                                       \
+                                                                                            \
+    return dst
+
 union dmemacc {
     unsigned char *byte;
     unsigned *dword;
@@ -11,53 +34,9 @@ union smemacc {
 };
 
 void *cldm_mmove(void *dst, void const* src, size_t size) {
-    union dmemacc d = {
-        .byte = dst
-    };
-    union smemacc s = {
-        .byte = src
-    };
-
-    size_t align = cldm_alignof(unsigned);
-    unsigned i = 0;
-
-    for(; i < size && ((((size_t)d.byte) % align) || (((size_t)s.byte) % align)); i++) {
-        *d.byte++ = *s.byte++;
-    }
-
-    for(; i + sizeof(*s.dword) <= size; i += sizeof(*s.dword)) {
-        *d.dword++ = *s.dword++;
-    }
-
-    for(; i < size; i++) {
-        *d.byte++ = *s.byte++;
-    }
-
-    return dst;
+    cldm_copy_bytes(dst, src, size);
 }
 
 void *cldm_mcpy(void *restrict dst, void const *restrict src, size_t size) {
-    union dmemacc d = {
-        .byte = dst
-    };
-    union smemacc s = {
-        .byte = src
-    };
-
-    size_t align = cldm_alignof(unsigned);
-    unsigned i = 0;
-
-    for(; i < size && ((((size_t)d.byte) % align) || (((size_t)s.byte) % align)); i++) {
-        *d.byte++ = *s.byte++;
-    }
-
-    for(; i + sizeof(*s.dword) <= size; i += sizeof(*s.dword)) {
-        *d.dword++ = *s.dword++;
-    }
-
-    for(; i < size; i++) {
-        *d.byte++ = *s.byte++;
-    }
-
-    return dst;
+    cldm_copy_bytes(dst, src, size);
 }
