bbb8f67bcdaab9c513bb306813bd00327851206d
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-03-13 09:12:22 +0100

    Fix issue where mmove did not handle overlap

--- cldm/cldm_mem.c
+++ cldm/cldm_mem.c
@@ -1,34 +1,37 @@
 #include "cldm_mem.h"
 
-#define cldm_copy_bytes(dst, src, size)                                                     \
-    union {                                                                                 \
-        unsigned char *byte;                                                                \
-        unsigned *dword;                                                                    \
-    } d = { .byte = dst };                                                                  \
-    union {                                                                                 \
-        unsigned char const *byte;                                                          \
-        unsigned const *dword;                                                              \
-    } s = { .byte = src };                                                                  \
-                                                                                            \
-    size_t align = cldm_alignof(unsigned);                                                  \
-    unsigned i = 0;                                                                         \
-                                                                                            \
-    for(; i < size && ((((size_t)d.byte) % align) || (((size_t)s.byte) % align)); i++) {    \
-        *d.byte++ = *s.byte++;                                                              \
-    }                                                                                       \
-    for(; i + sizeof(*s.dword) <= size; i += sizeof(*s.dword)) {                            \
-        *d.dword++ = *s.dword++;                                                            \
-    }                                                                                       \
-    for(; i < size; i++) {                                                                  \
-        *d.byte++ = *s.byte++;                                                              \
-    }                                                                                       \
-                                                                                            \
-    return dst
 
 void *cldm_mmove(void *dst, void const* src, size_t size) {
-    cldm_copy_bytes(dst, src, size);
+    unsigned char *d = dst;
+    unsigned char const *s = src;
+    while(size--) {
+        *(unsigned char *)d++ = *(unsigned char *)s++;
+    }
+    return dst;
 }
 
 void *cldm_mcpy(void *restrict dst, void const *restrict src, size_t size) {
-    cldm_copy_bytes(dst, src, size);
+    union {
+        unsigned char *byte;
+        unsigned *dword;
+    } d = { .byte = dst };
+    union {
+        unsigned char const *byte;
+        unsigned const *dword;
+    } s = { .byte = src };
+
+    size_t align = cldm_alignof(unsigned);
+    unsigned i = 0;
+
+    for(; i < size && ((((size_t)d.byte) % align) || (((size_t)s.byte) % align)); i++) {
+        *d.byte++ = *s.byte++;
+    }
+    for(; i + sizeof(*s.dword) <= size; i += sizeof(*s.dword)) {
+        *d.dword++ = *s.dword++;
+    }
+    for(; i < size; i++) {
+        *d.byte++ = *s.byte++;
+    }
+
+    return dst;
 }
