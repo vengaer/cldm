caf5d4b8ac435fa7bdeb90148cd00bb5aa42b3b3
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-03-20 20:53:18 +0100

    Add cldm_ntbschr test

--- test/unit/ntbs_test.c
+++ test/unit/ntbs_test.c
@@ -1,5 +1,12 @@
 #include <cldm/cldm.h>
+#include <cldm/cldm_log.h>
+#include <cldm/cldm_ntbs.h>
 
 TEST(cldm_ntbschr) {
-
+    char const *str = "a string with spaces";
+    ASSERT_EQ(cldm_ntbschr((char *)str, ' '), str + 1);
+    str = "some/path/to/a/file";
+    ASSERT_EQ(cldm_ntbschr((char *)str, '/'), str + 4);
+    str = "a string without slashes";
+    ASSERT_EQ(cldm_ntbschr((char *)str, '/'), 0);
 }
