8da1c1a8adc51d044ed2aff042933a9aa4ba90f2
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-03-20 21:06:48 +0100

    Add ntbscpy test

--- test/unit/ntbs_test.c
+++ test/unit/ntbs_test.c
@@ -2,6 +2,10 @@
 #include <cldm/cldm_log.h>
 #include <cldm/cldm_ntbs.h>
 
+#include <string.h>
+
+#include <sys/types.h>
+
 TEST(cldm_ntbschr) {
     char const *str = "a string with spaces";
     ASSERT_EQ(cldm_ntbschr((char *)str, ' '), str + 1);
@@ -24,4 +28,22 @@ TEST(cldm_ntbslen) {
     ASSERT_EQ(cldm_ntbslen("a string"), 8);
     ASSERT_EQ(cldm_ntbslen("another string"), 14);
     ASSERT_EQ(cldm_ntbslen("a"), 1);
+}
+
+TEST(cldm_ntbscpy) {
+    enum { BUFSIZE = 64 };
+    char src[BUFSIZE];
+    char dst[BUFSIZE];
+
+    strcpy(src, "a string");
+
+    ASSERT_EQ(cldm_ntbscpy(dst, src, sizeof(src)), (ssize_t)strlen(src));
+    ASSERT_EQ(strcmp(dst, src), 0);
+
+    ASSERT_LT(cldm_ntbscpy(dst, src, 4), 0);
+    ASSERT_EQ(dst[3], 0);
+    ASSERT_EQ(strcmp(dst, "a s"), 0);
+
+    ASSERT_EQ(cldm_ntbscpy(dst, src, 9), (ssize_t)strlen(src));
+    ASSERT_EQ(dst[strlen(src)], 0);
 }
