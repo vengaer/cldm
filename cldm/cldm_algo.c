3d856d1bfdd3aafddc1f232cc7382506ed6ffe66
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-04-04 21:11:34 +0200

    Avoid superfluous calls to memcpy

--- cldm/cldm_algo.c
+++ cldm/cldm_algo.c
@@ -12,6 +12,7 @@ bool cldm_is_prime(size_t num) {
         return false;
     }
 
+    /* Remaining primes are on form 6n +/- 1 */
     for(size_t div = 6u; div * div - 2 * div < num; div += 6u) {
         if(!(num % (div - 1)) || !(num % div + 1)) {
             return false;
@@ -37,20 +38,18 @@ ssize_t cldm_stable_partition(void *data, size_t elemsize, size_t nelems, bool(*
     for(unsigned i = 0; i < nelems; i++) {
         elem = (unsigned char *)data + i * elemsize;
         if(predicate(elem)) {
+            /* Write to lower half */
             memcpy(arr + elemsize * st++, elem, elemsize);
         }
         else {
+            /* Write to upper half */
             memcpy(arr + (nelems + ust++) * elemsize, elem, elemsize);
         }
     }
 
-    for(unsigned i = 0; i < st; i++) {
-        memcpy((unsigned char *)data + i * elemsize, arr + i * elemsize, elemsize);
-    }
-
-    for(unsigned d = st, i = 0; i < ust; i++, d++) {
-        memcpy((unsigned char *)data + d * elemsize, arr + (i + nelems) * elemsize, elemsize);
-    }
+    /* Write back */
+    memcpy(data, arr, st * elemsize);
+    memcpy((unsigned char *)data + st * elemsize, arr + nelems * elemsize, ust * elemsize);
 
     free(arr);
 
