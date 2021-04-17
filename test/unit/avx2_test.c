cad05074693177c220d55f5c05d7ca9098b1ea46
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-04-17 20:28:12 +0200

    Add unit test for cldm_avx2_strscpy

---
+++ test/unit/avx2_test.c
@@ -0,0 +1,24 @@
+#include <cldm/cldm.h>
+
+#include <stddef.h>
+#include <string.h>
+
+extern long long cldm_avx2_strscpy(char *restrict dst, char const *restrict src, size_t dstsize);
+
+TEST(cldm_avx2_strscpy) {
+    enum { SIZE = 256 };
+    char dst[SIZE];
+    char const *src = "Fear Inoculum Pneuma Litanie contre la Peur Invincible Legion Inculant Descending Culling Voices Chocolate Chip Trip 7empest Mockingbeat";
+    ASSERT_EQ(cldm_avx2_strscpy(dst, src, sizeof(dst)), (ssize_t)strlen(src));
+    ASSERT_STREQ(dst, src);
+}
+
+TEST(cldm_avx2_strscpy_termination) {
+    enum { SIZE = 16 };
+    char dst[SIZE];
+    char const *src = "Hear lighting flash, but don't blink. Misleading. Tranquility ruse. You're gonna happen again. That's what I think";
+
+    ASSERT_EQ(cldm_avx2_strscpy(dst, src, sizeof(dst)), -7);
+    ASSERT_STRNEQ(src, dst, sizeof(dst) - 1);
+    ASSERT_EQ(dst[sizeof(dst) - 1], 0);
+}
