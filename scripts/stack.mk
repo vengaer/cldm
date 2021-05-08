0924b1542571a8fda2e24e0636bafaf33fcdd97b
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-05-08 22:56:31 +0200

    Fix issue where modules required by multiple targets were dropper by stack

--- scripts/stack.mk
+++ scripts/stack.mk
@@ -1,17 +1,25 @@
-stack_top_symb := :
+stack_top_symb  := :
+stack_separator := %
+
+cat = $(firstword $(1))$(if $(filter-out $(firstword $(1)),$(1)),$(2)$(call cat,$(filter-out $(firstword $(1)),$(1)),$(2)))
+
+# $(call stack-top-raw, STACK_NAME)
+define stack-top-raw
+$(subst $(stack_top_symb),,$(firstword $($(1))))
+endef
 
 # $(call stack-top, STACK_NAME)
 define stack-top
-$(subst $(stack_top_symb),,$(firstword $($(1))))
+$(subst $(stack_separator), ,$(call stack-top-raw,$(1)))
 endef
 
 # $(call stack-push, STACK_NAME, VALUE)
 define stack-push
-$(eval $(1) := $(stack_top_symb)$(strip $(2)) $(subst $(stack_top_symb),,$($(1))))
+$(eval $(1) := $(stack_top_symb)$(strip $(call cat,$(2),$(stack_separator))) $(subst $(stack_top_symb),,$($(1))))
 endef
 
 # $(call stack-pop, STACK_NAME)
 define stack-pop
-$(eval __top := $(call stack-top,$(1)))
+$(eval __top := $(call stack-top-raw,$(1)))
 $(eval $(1)  := $(stack_top_symb)$(subst $(stack_top_symb)$(__top) ,,$($(1))))
 endef
