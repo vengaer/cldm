4fd1962ceefb1c03d30c99e4f0878b5bf5a47212
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-05-22 12:44:01 +0200

    Add unit tests for avx2 memset

--- test/unit/avx2_test.c
+++ test/unit/avx2_test.c
@@ -7,6 +7,7 @@
 #ifdef CLDM_HAS_AVX2
 
 extern long long cldm_avx2_strscpy(char *restrict dst, char const *restrict src, unsigned long long dstsize);
+extern void *cldm_avx2_memset(void *dst, int v, unsigned long long n);
 
 TEST(cldm_avx2_strscpy) {
     enum { SIZE = 256 };
@@ -40,6 +41,42 @@ TEST(cldm_avx2_strscpy_dstsize_zero) {
     char const *src = "asdf";
     ASSERT_EQ(cldm_avx2_strscpy(dst, src, 0), -7);
     ASSERT_EQ(dst[0], 0x18);
+}
+
+TEST(cldm_avx2_memset) {
+    enum { SIZE = 4096 };
+    char dst[SIZE] = { 0 };
+    char ref[SIZE];
+    memset(ref, 0xff, sizeof(ref));
+    ASSERT_EQ(cldm_avx2_memset(dst, 0xff, sizeof(dst)), dst);
+    ASSERT_EQ(memcmp(dst, ref, sizeof(dst)), 0);
+}
+
+TEST(cldm_avx2_memset_unaligned) {
+    enum { SIZE = 2048 };
+    struct {
+        unsigned long long p0;
+        unsigned char p1;
+        char dst[SIZE];
+    } d;
+    char ref[SIZE];
+
+    memset(d.dst, 0, sizeof(d.dst));
+    memset(ref, 0xff, sizeof(ref));
+
+    ASSERT_EQ(cldm_avx2_memset(d.dst, 0xff, sizeof(d.dst)), d.dst);
+    ASSERT_EQ(memcmp(d.dst, ref, sizeof(d.dst)), 0);
+}
+
+TEST(cldm_avx2_memset_odd_size) {
+    enum { SIZE = 3999 };
+    char dst[SIZE] = { 0 };
+    char ref[SIZE];
+
+    memset(ref, 0xff, sizeof(ref));
+
+    ASSERT_EQ(cldm_avx2_memset(dst, 0xff, sizeof(dst)), dst);
+    ASSERT_EQ(memcmp(dst, ref, sizeof(dst)), 0);
 }
 
 #endif /* CLDM_HAS_AVX2 */
