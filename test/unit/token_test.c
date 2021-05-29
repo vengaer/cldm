0e6536089241b13a81e3532912c51543ebc20d8e
Vilhelm Engström <vilhelm.engstrom@tuta.io>
2021-05-29 22:33:49 +0200

    Add unit tests for token macros

---
+++ test/unit/token_test.c
@@ -0,0 +1,27 @@
+#include <cldm/cldm.h>
+#include <cldm/cldm_macro.h>
+#include <cldm/cldm_token.h>
+
+TEST(token_void) {
+    ASSERT_TRUE(cldm_token_void(void));
+    ASSERT_FALSE(cldm_token_void(int));
+    ASSERT_FALSE(cldm_token_void(void *));
+}
+
+TEST(token_0) {
+    ASSERT_TRUE(cldm_token_0(0));
+    ASSERT_FALSE(cldm_token_0(1));
+    ASSERT_FALSE(cldm_token_0(asdf));
+}
+
+TEST(token_1) {
+    ASSERT_TRUE(cldm_token_1(1));
+    ASSERT_FALSE(cldm_token_1(0));
+    ASSERT_FALSE(cldm_token_1(asdf));
+}
+
+TEST(repeat_token) {
+#define m(_, v)   ASSERT_EQ(v,30)
+    cldm_map_separate(m, 8, ;, cldm_repeat_token(30, 8, 8));
+#undef m
+}
