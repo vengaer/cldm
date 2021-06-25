07801bda3552cf88c2f3391f121b75bbd1ab3b51
Vilhelm <vilhelm.engstrom@tuta.io>
2021-06-25 13:49:17 +0200

    Add noreturn detection

--- cldm/cldm_token.h
+++ cldm/cldm_token.h
@@ -7,12 +7,15 @@
 #define cldm_probe_void_void() cldm_generic_probe()
 #define cldm_probe_1_1() cldm_generic_probe()
 #define cldm_probe_0_0() cldm_generic_probe()
+#define cldm_probe_noreturn_noreturn() cldm_generic_probe()
+#define cldm_probe__Noreturn__Noreturn() cldm_generic_probe()
 
 #define cldm_match_token(token, x) cldm_chk(cldm_cat_expand(cldm_cat_expand(cldm_cat_expand(cldm_probe_,token),_),x)())
 
 #define cldm_token_void(x) cldm_match_token(void, x)
 #define cldm_token_0(x) cldm_match_token(0,x)
 #define cldm_token_1(x) cldm_match_token(1,x)
+#define cldm_token_noreturn(x) (cldm_match_token(_Noreturn,x) | cldm_match_token(noreturn,x))
 
 #define cldm_repeat_token_recurse1(...)
 #define cldm_defer_repeat_token_recursion() cldm_repeat_token_recurse
