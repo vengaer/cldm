332314cb155bb843ccee436e925edc8bfc37c2ad
Vilhelm <vilhelm.engstrom@tuta.io>
2021-06-25 23:49:19 +0200

    Add jump address stack

---
+++ cldm/cldm_jmpstack.c
@@ -0,0 +1,37 @@
+#include "cldm_byteseq.h"
+#include "cldm_jmpstack.h"
+#include "cldm_log.h"
+
+jmp_buf **cldm_jmpstack_storage(struct cldm_jmpstack *stack);
+jmp_buf *cldm_jmpstack_top(struct cldm_jmpstack *stack);
+void cldm_jmpstack_init(struct cldm_jmpstack *stack);
+void cldm_jmpstack_free(struct cldm_jmpstack *stack);
+void cldm_jmpstack_pop(struct cldm_jmpstack *stack);
+size_t cldm_jmpstack_size(struct cldm_jmpstack const *stack);
+size_t cldm_jmpstack_capacity(struct cldm_jmpstack const *stack);
+
+bool cldm_jmpstack_push(struct cldm_jmpstack *restrict stack, void *restrict jmpbuf) {
+    void *buf;
+    if(stack->size == stack->capacity) {
+        if(stack->capacity == cldm_arrsize(stack->s_un.stat)) {
+            buf = malloc((stack->capacity << 1u) * sizeof(*stack->s_un.dyn));
+            if(!buf) {
+                cldm_err("Could not allocate jump stack");
+                return false;
+            }
+            cldm_memcpy(buf, stack->s_un.stat, sizeof(stack->s_un.stat));
+        }
+        else {
+            buf = realloc(stack->s_un.dyn, (stack->capacity << 1u) * sizeof(*stack->s_un.dyn));
+            if(!buf) {
+                cldm_err("Could not allocate jump stack");
+                free(stack->s_un.dyn);
+                return false;
+            }
+        }
+        stack->s_un.dyn = buf;
+        stack->capacity <<= 1u;
+    }
+    cldm_jmpstack_storage(stack)[stack->size++] = jmpbuf;
+    return true;
+}
---
+++ cldm/cldm_jmpstack.h
@@ -0,0 +1,57 @@
+#ifndef CLDM_JMPSTACK_H
+#define CLDM_JMPSTACK_H
+
+#include "cldm_macro.h"
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include <setjmp.h>
+
+enum { CLDM_JMPSTACK_STATIC_SIZE = 32 };
+
+struct cldm_jmpstack {
+    union {
+        jmp_buf *stat[CLDM_JMPSTACK_STATIC_SIZE];
+        jmp_buf **dyn;
+    } s_un ;
+    size_t size;
+    size_t capacity;
+};
+
+inline jmp_buf **cldm_jmpstack_storage(struct cldm_jmpstack *stack) {
+    return stack->capacity > cldm_arrsize(stack->s_un.stat) ? stack->s_un.dyn : stack->s_un.stat;
+}
+
+inline void cldm_jmpstack_init(struct cldm_jmpstack *stack) {
+    *stack = (struct cldm_jmpstack) {
+        .size = 0,
+        .capacity = cldm_arrsize(stack->s_un.stat)
+    };
+}
+
+inline void cldm_jmpstack_free(struct cldm_jmpstack *stack) {
+    if(stack->size > cldm_arrsize(stack->s_un.stat)) {
+        free(stack->s_un.dyn);
+    }
+}
+
+bool cldm_jmpstack_push(struct cldm_jmpstack *restrict stack, void *restrict addr);
+
+inline jmp_buf *cldm_jmpstack_top(struct cldm_jmpstack *stack) {
+    return cldm_jmpstack_storage(stack)[stack->size - 1];
+}
+
+inline void cldm_jmpstack_pop(struct cldm_jmpstack *stack) {
+    --stack->size;
+}
+
+inline size_t cldm_jmpstack_size(struct cldm_jmpstack const *stack) {
+    return stack->size;
+}
+
+inline size_t cldm_jmpstack_capacity(struct cldm_jmpstack const *stack) {
+    return stack->capacity;
+}
+
+#endif /* CLDM_JMPSTACK_H */
